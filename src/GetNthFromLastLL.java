a295e82e27031c8e98d9835febe7321d9c779c55
Kunal Kheeva <98793512+kunalkheeva1@users.noreply.github.com>
2022-06-24 18:56:21 -0700

    created a method to get the data of the nth node form the end of the linked list

--- src/GetNthFromLastLL.java
+++ src/GetNthFromLastLL.java
@@ -8,6 +8,25 @@ public class GetNthFromLastLL {
             this.next = null;
         }
     }
+    public static int getNthFromLastLL(Node head, int n){
+        if(head == null){
+            return -1;
+        }
+        int size = 0;
+        Node current = head;
+        while( current != null){
+            size++;
+            current = current.next;
+        }
+        if(size<n){
+            return -1;
+        }
+        int count = size - n;
+        while(count >0){
+            count --;
+            head = head.next;
+        }return head.data;
+    }
 
 
 
