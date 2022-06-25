6534209c0fed3790409f1c8d60600c6ca23cd9cd
Kunal Kheeva <98793512+kunalkheeva1@users.noreply.github.com>
2022-06-24 18:48:49 -0700

    created a node class

--- src/GetNthFromLastLL.java
+++ src/GetNthFromLastLL.java
@@ -1,2 +1,18 @@
 public class GetNthFromLastLL {
+
+    static class Node{
+        int data;
+        Node next;
+        Node(int data){
+            this.data = data;
+            this.next = null;
+        }
+    }
+
+
+
+
+    public static void main(String[] args) {
+
+    }
 }
